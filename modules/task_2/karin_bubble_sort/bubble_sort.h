fb98a2b85c81a9104c7e5ddc51c332e03d5e7efb
Timofey <karin.timofej@yandex.ru>
2019-11-16 00:24:29 +0300

    for travis7

--- modules/task_2/karin_bubble_sort/bubble_sort.h
+++ modules/task_2/karin_bubble_sort/bubble_sort.h
@@ -1,4 +1,5 @@
 // Copyright 2019 Karin Timofey
+#include <vector>
 #ifndef MODULES_TASK_2_KARIN_BUBBLE_SORT_BUBBLE_SORT_H_
 #define MODULES_TASK_2_KARIN_BUBBLE_SORT_BUBBLE_SORT_H_
 
@@ -10,4 +11,4 @@ std::vector<int> GetMaxVec(const std::vector<int>& local_vec, int local_size,
                             const std::vector<int>& neig_vec, int neig_size);
 std::vector<int> ParBubbleSort(const std::vector<int>& vect);
 
-#endif  // MODULES_TASK_1_KARIN_BUBBLE_SORT_BUBBLE_SORT_H_
+#endif  // MODULES_TASK_2_KARIN_BUBBLE_SORT_BUBBLE_SORT_H_
